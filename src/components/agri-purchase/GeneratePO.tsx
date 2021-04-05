5e9b39ec3f5eda8e5be2621ad4c736cf83a6cd26
Shlok D. Mange <shlok_mange@protonmail.com>
2021-04-05 16:21:56 +0400

    Edited Product Title

--- src/components/agri-purchase/GeneratePO.tsx
+++ src/components/agri-purchase/GeneratePO.tsx
@@ -1,6 +1,20 @@
+import { faArrowAltCircleDown, faArrowDown, faDownload, faFileExcel, faSortDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const orderList = [
+    {
+        name: 'ASJKIWBW8935',
+        by: 'Alex Lee',
+        time: 'Mon, 13 Apr, 10:42 AM',
+        file: 'ASJKIWBW8935.csv'
+    },
+    {
+        name: 'IADSAHSB7863',
+        by: 'Alex Lee',
+        time: 'Mon, 13 Apr, 10:42 AM',
+        file: 'IADSAHSB7863.csv'
+    },
     {
         name: 'ASJKIWBW8935',
         by: 'Alex Lee',
@@ -17,7 +31,7 @@ const orderList = [
 
 const GeneratePO: React.FC = () => {
     return(
-        <section className="py-4">
+        <section className="py-4 space-y-2">
             <div className="flex items-center space-x-16 border-b-2 ">
                 <p>Executing Agri-Input Orders</p>
                 <p>Batched By</p>
@@ -28,13 +42,20 @@ const GeneratePO: React.FC = () => {
                         return(
                             <div 
                                 key={index} 
-                                className="flex w-full space-x-12 bg-gray-600 text-gray-200 px-4 py-3"
+                                className="flex w-full space-x-12 bg-gray-200 text-gray-600 px-4 py-3"
                             >
-                                <div className="w-1/4">
+                                <div className="w-1/4 flex items-center justify-between pr-8">
                                     <p>{order.name}</p>
+                                    <FontAwesomeIcon icon={faSortDown}/>
                                 </div>
-                                <div>
-                                    <p>{order.by}</p>
+                                <div className="">
+                                    <p className="text-sm">{order.by}</p>
+                                    <p className="text-xs">{order.time}</p>
+                                </div>
+                                <div className="flex items-center space-x-4 w-1/4 justify-between">
+                                    <FontAwesomeIcon icon={faFileExcel} className="text-green-700"/>
+                                    <p>{order.file}</p>
+                                    <FontAwesomeIcon icon={faDownload}/>
                                 </div>
                             </div>
                         )
